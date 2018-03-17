0f154856a4107acd5d6e37e09110afa24eac7da1
apodlawski <apodlawski@P109-15>
2018-03-17 08:45:20 +0100

    lint refactor

--- lab2/module.js
+++ lab2/module.js
@@ -1,41 +1,41 @@
-var MechanicModule = {}
-var CarModule = {}
-var ClientModule = {}
+var MechanicModule = {};
+var CarModule = {};
+var ClientModule = {};
 
 CarModule = (function () {
 
     var CarInterface = {
-        brand: 'BMW',
-        model: 'E90',
-        productionYear: '2011',
+        brand: "BMW",
+        model: "E90",
+        productionYear: "2011",
         parts: {
             engine: {
-                price: '20000',              
-                repairPriority : 'high'
+                price: "20000",              
+                repairPriority : "high"
             },
             tires: {
-                price: '3000',
-                repairPriority : 'low'
+                price: "3000",
+                repairPriority : "low"
             },
             oil: {
-                price: '300',
-                repairPriority : 'high'
+                price: "300",
+                repairPriority : "high"
             },
             ligths: {
-                price: '1500',
-                repairPriority : 'low'
+                price: "1500",
+                repairPriority : "low"
             },
             transmission: {
-                price: '7000',
-                repairPriority : 'medium'
+                price: "7000",
+                repairPriority : "medium"
             },
         },
 
         GetCarInformations() {
-            console.log('Car: ' +
-                '\n brand: ' + this.brand +
-                '\n model: ' + this.model +
-                '\n production year: ' + this.productionYear)
+            console.log("Car: " +
+                "\n brand: " + this.brand +
+                "\n model: " + this.model +
+                "\n production year: " + this.productionYear)
         }
     }
 
@@ -46,17 +46,17 @@ CarModule = (function () {
 ClientModule = (function () {
 
     var ClientInterface = {
-        name: 'Jan',
-        surname: 'Kowalski',
-        city: 'Gdańsk',
-        street: 'Naprawcza',
+        name: "Jan",
+        surname: "Kowalski",
+        city: "Gdańsk",
+        street: "Naprawcza",
         discount : 10,
 
         GetClientInformations: function () {
-            console.log('Client: ' +
-                '\n name: ' + this.name +
-                '\n surname: ' + this.surname +
-                '\n city: ' + this.city)
+            console.log("Client: " +
+                "\n name: " + this.name +
+                "\n surname: " + this.surname +
+                "\n city: " + this.city)
         }
     }
 
@@ -66,8 +66,8 @@ ClientModule = (function () {
 MechanicModule = (function () {
 
     var MechanicInterface = {
-        name: 'Adrian',
-        specialization: 'Cars',
+        name: "Adrian",
+        specialization: "Cars",
 
         GetOrder(car, client) {
             car.GetCarInformations();
@@ -77,27 +77,27 @@ MechanicModule = (function () {
         Overwiev(car){
             var highPriorityParts = 0;
             Object.keys(car.parts).forEach(function(part){
-                if(car.parts[part].repairPriority === 'high'){
+                if(car.parts[part].repairPriority === "high"){
                     highPriorityParts += 1;
                 }
             });
 
-            console.log('You need to repair ' + highPriorityParts + ' parts as fast as possible.')
+            console.log("You need to repair " + highPriorityParts + " parts as fast as possible.")
         },
 
         CalcDiscount(car, client){
             var discount = function(val){
                 var finalPrice = val - val * client.discount/100;
-                console.log('Price after discount ' + finalPrice)
+                console.log("Price after discount " + finalPrice)
             }
 
             Object.keys(car.parts).map(function(part){
-                discount(car.parts[part]['price'])
+                discount(car.parts[part]["price"])
             })
         },
 
         GetInformationAboutMechanic() {
-            console.log('Name: ' + this.name + ', Specialization: ' + this.specialization)
+            console.log("Name: " + this.name + ", Specialization: " + this.specialization)
         }
     }
 
